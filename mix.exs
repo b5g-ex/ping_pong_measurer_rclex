7fbfab119222e9b2058174c9148b71fb16c118eb
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-04-03 17:06:10 +0900

    Update :rclex from 0.7.2 to 0.10.0

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule PingPongMeasurerRclex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rclex, "~> 0.7.2"},
+      {:rclex, "~> 0.10.0"},
       {:nimble_csv, "~> 1.1"},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:flow, "~> 1.0"}

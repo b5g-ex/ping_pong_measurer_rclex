cc4833d758a480409e7822a7dbc1d8905dafac1f
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-04-03 18:01:48 +0900

    Add start_pong_processes/3

--- lib/ping_pong_measurer_rclex.ex
+++ lib/ping_pong_measurer_rclex.ex
@@ -2,4 +2,10 @@ defmodule PingPongMeasurerRclex do
   @moduledoc """
   Documentation for `PingPongMeasurerRclex`.
   """
+
+  alias PingPongMeasurerRclex.Pong
+
+  def start_pong_processes(node_count, ping_pub, ping_sub) do
+    Pong.start_link(node_count: node_count, ping_pub: ping_pub, ping_sub: ping_sub)
+  end
 end
---
+++ lib/ping_pong_measurer_rclex/pong.ex
@@ -0,0 +1,49 @@
+defmodule PingPongMeasurerRclex.Pong do
+  use GenServer
+
+  alias Rclex.Pkgs.StdMsgs
+
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(args) do
+    node_count = Keyword.fetch!(args, :node_count)
+    ping_pub = Keyword.fetch!(args, :ping_pub)
+    ping_sub = Keyword.fetch!(args, :ping_sub)
+
+    node_names =
+      for index <- 0..(node_count - 1) do
+        node_name = "pong" <> String.pad_leading("#{index}", 3, "0")
+
+        :ok = Rclex.start_node(node_name)
+        node_name
+      end
+
+    for {node_name, index} <- Enum.with_index(node_names) do
+      ping_topic =
+        case ping_pub do
+          :single -> "/ping"
+          :multiple -> "/ping" <> String.pad_leading("#{index}", 3, "0")
+        end
+
+      pong_topic =
+        case ping_sub do
+          :single -> "/pong"
+          :multiple -> "/pong" <> String.pad_leading("#{index}", 3, "0")
+        end
+
+      :ok =
+        Rclex.start_subscription(
+          &Rclex.publish(&1, pong_topic, node_name),
+          StdMsgs.Msg.String,
+          ping_topic,
+          node_name
+        )
+
+      :ok = Rclex.start_publisher(StdMsgs.Msg.String, pong_topic, node_name)
+    end
+
+    {:ok, %{node_count: node_count}}
+  end
+end

8cf8714771c0ea7a764ffda6bc33932dfe55f631
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-04-24 09:27:08 +0900

    debug レベルのログをコンパイル時にパージさせる設定を追加

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,9 @@ import Config
 config :logger, level: :info
 
 config :rclex, ros2_message_types: ["std_msgs/msg/String"]
+
+config :logger,
+  compile_time_purge_matching: [
+    [application: :rclex],
+    [level_lower_than: :info]
+  ]

38e53d6e996b0646bbf8ddd363303d9e2ad2315c
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-05-01 13:24:51 +0900

    Change rclex branch

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule PingPongMeasurerRclex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rclex, git: "https://github.com/rclex/rclex.git", branch: "pojiro/trial"},
+      {:rclex, git: "https://github.com/rclex/rclex.git", branch: "pojiro/fixes"},
       {:nimble_csv, "~> 1.1"},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:flow, "~> 1.0"},

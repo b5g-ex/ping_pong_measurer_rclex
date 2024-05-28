d82ec38c7356b0a00e6c62ce7c27416b493a1f0f
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-05-29 08:44:49 +0900

    Bump version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PingPongMeasurerRclex.MixProject do
   def project do
     [
       app: :ping_pong_measurer_rclex,
-      version: "0.1.0",
+      version: "1.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
